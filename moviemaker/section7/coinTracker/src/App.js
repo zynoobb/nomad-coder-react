e7e77bb49b61b4fd8e7bbbff3cc686b6d5dc5a7f
zynoobb <jw9491@gmail.com>
2024-10-11 04:59:24 +0900

    Done : section7 fetch 요청 학습

---
+++ moviemaker/section7/coinTracker/src/App.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+function App() {
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === '') {
+      return;
+    }
+    setToDo('');
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
+
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do"
+        ></input>
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
